a32e206d36820842ad21eb362d6b15c8ec2fc588
MariusGabrielPetre <petre.marius93@gmail.com>
2025-04-06 14:51:16 +0300

    Added method to remove products by name

--- product_manager.py
+++ product_manager.py
@@ -12,3 +12,6 @@ class ProductManager:
     def calculate_total_value(self):
         total = sum(p.price * p.quantity for p in self.products)
         print(f"Total inventory value: {total}")
+
+    def remove_product(self, name):
+        self.products = [p for p in self.products if p.name != name]
